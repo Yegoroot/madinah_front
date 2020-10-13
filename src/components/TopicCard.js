9ba5334f4f11e5fb9be4db27ef37f8af22859289
Yegor Kirsanov <kirsanov.yegor@gmail.com>
2020-10-13 21:40:13 +0300

    emoji to topic

--- src/components/TopicCard.js
+++ src/components/TopicCard.js
@@ -59,7 +59,7 @@ function TopicCard({
           to={`${PUBLIC_PROGRAMS_URL}/${programId}/topics/${topic.id}`}
           variant="h2"
         >
-          {topic.title}
+         🌴 {topic.title}
         </Link>
         { !user || !document_is_my_own(user, topic.user._id) || !perm_work_with_program(role) ? null
           : (
