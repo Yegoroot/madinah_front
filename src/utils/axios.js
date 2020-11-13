73dc4af04cde8004645de87310bc202940880668
Yegor Kirsanov <kirsanov.yegor@gmail.com>
2020-11-14 02:43:47 +0300

    one error message was translated

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,12 +2,13 @@ import axios from 'axios'
 import store from 'src/store'
 import { enqueueSnackbar } from 'src/slices/alert'
 import ObjectID from 'bson-objectid'
+import i18n from 'i18next'
 
 const instanceWithMock = axios.create()
 
 const errorHandler = (err) => {
   store.dispatch(enqueueSnackbar({
-    message: err.message,
+    message: i18n.t(`error.${err.message}`),
     options: {
       autoHideDuration: 4000,
       key: ObjectID.generate(),
