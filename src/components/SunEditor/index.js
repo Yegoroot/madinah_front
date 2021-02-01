1c6eab93e216848cf15d1cd954411e5f432c7d04
Amiroot <easywayroot@gmail.com>
2021-02-01 12:40:11 +0300

    new version sunEditor bug fix

--- src/components/SunEditor/index.js
+++ src/components/SunEditor/index.js
@@ -2,9 +2,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react'
 import SunEditor from 'suneditor-react'
-import {
-  formatBlock, list, link, fontColor, hiliteColor
-} from 'suneditor/src/plugins'
 import './style.css'
 
 import CodeMirror from 'codemirror'
@@ -24,7 +21,9 @@ const Editor = ({ onChange, content }) => {
         minHeight: 200,
         formats: ['p', 'div', 'blockquote', 'h3', 'h4'],
         colorList: [['#f44336', '#3949ab', '#1c2025'], ['#4caf50', '#e6e5e8', '#8a85ff']],
-        buttonList: [[fontColor, hiliteColor, 'bold', link], [list, formatBlock], ['codeView', 'undo', 'redo']
+        buttonList: [
+          ['strike', 'fontColor', 'hiliteColor', 'bold', 'link'],
+          ['list', 'formatBlock'], ['codeView', 'undo', 'redo']
         ]
       }}
       lang={settings.lang}
