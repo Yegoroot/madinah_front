b094b41a17651c6cf8e51064e670787594c1ad86
Amiroot <easywayroot@gmail.com>
2021-03-04 02:11:20 +0300

    +

--- src/components/SocialButtons.tsx
+++ src/components/SocialButtons.tsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core'
 import { useTranslation } from 'react-i18next'
-import { HOST } from 'src/constants'
-import axios from 'src/utils/axios'
-import WindowOpener from 'react-window-opener'
 
 const useStyles = makeStyles((/* theme */) => ({
   googleButton: {
@@ -31,26 +28,18 @@ const useStyles = makeStyles((/* theme */) => ({
 export const GoogleButton = () => {
   const classes = useStyles()
   const { t } = useTranslation()
-  const childResponse = (e) => console.log('s--', e)
-  // const loginByGoogle = async () => {
-  //   // const res = await axios.get('https://madinah.best/api/v1/auth/google')
-  //   // console.log(res)
-  //   const a = window.open('https://madinah.best/api/v1/auth/google', '_self', '', true)
-  // }
+  const handleGoogleClick = () => {
+    console.log('user')
+    window.open('http://localhost/api/v1/auth/google', '_self')
+  }
   return (
-    <WindowOpener
-      url="/popUp-URL"
-      bridge={childResponse}
+    <div
+      className={classes.googleButton}
+      onClick={handleGoogleClick}
     >
-      <div
-        className={classes.googleButton}
-        // onClick={loginByGoogle}
-      >
-        <span className={classes.googleLetter}>G</span>
-        {t('btn.Sign in with google')}
-
-      </div>
-    </WindowOpener>
+      <span className={classes.googleLetter}>G</span>
+      {t('btn.Sign in with google')}
+    </div>
   )
 }
 
